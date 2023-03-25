413d8a63a79cacd5a314fe8b16bfa07372916c01
croosheck <jack150tw@gmail.com>
2023-03-25 18:30:08 +0100

    Empty feed message, if there's no results being found (with the country name searched specified)

--- src/sections/content/news/Feed.js
+++ src/sections/content/news/Feed.js
@@ -4,17 +4,30 @@ import Article from "./article/Article";
 import { useDispatch, useSelector } from "react-redux";
 import { getArticlesData } from "../../../utils/getArticlesData";
 import { setCountryData } from "../../../redux/slice";
-import { memo, useEffect } from "react";
+import { memo, useEffect, useState } from "react";
 import { DUMMY_ARTICLES } from "../../../appConfig";
 import { AnimatePresence, motion } from "framer-motion";
 
 function Feed() {
+	const [failedCountry, setFailedCountry] = useState();
 	const { id } = useParams();
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		async function showArticlesHandler() {
 			const data = await getArticlesData(id);
+
+			if (data.totalResults === 0) {
+				async function getCountryName(code) {
+					const response = await fetch(
+						`https://restcountries.com/v3.1/alpha/${code}`
+					);
+					const countryData = await response.json();
+					setFailedCountry(countryData[0].name.common);
+				}
+
+				getCountryName(id);
+			}
 
 			if (data.status === "error") {
 				dispatch(
@@ -44,6 +57,29 @@ function Feed() {
 	const { countryData, currentLayout } = useSelector(
 		(state) => state.newsReducer
 	);
+
+	if (countryData.totalResults === 0) {
+		return (
+			<AnimatePresence>
+				<motion.div
+					key={id}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1, transition: { duration: 0.4 } }}
+					exit={{ opacity: 0, transition: { duration: 0.1 } }}
+					className={`articles-feed-container ${
+						currentLayout === "list" ? "articles-feed-container-list" : ""
+					}`}
+				>
+					{
+						<h2 id="country-failed">
+							No articles has been found{" "}
+							{!!failedCountry && `for ${failedCountry}`}
+						</h2>
+					}
+				</motion.div>
+			</AnimatePresence>
+		);
+	}
 
 	return (
 		<AnimatePresence>
